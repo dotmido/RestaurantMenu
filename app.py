898261090425e0d63c9a4ef0b148260868ccd70c
Mohammed Mustafa <mjavax@gmail.com>
2018-12-07 15:59:02 +0300

    add file app.py with routes

---
+++ app.py
@@ -0,0 +1,57 @@
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db?check_same_thread=False')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+@app.route('/')
+def restaurants():
+    return 'List all restaurants'
+
+
+@app.route('/restaurant/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    return 'List specific restaurant Menu'
+
+
+@app.route('/restaurant/new/')
+def newRestaurant():
+    return 'create new restaurant'
+
+
+@app.route('/restaurant/<int:restaurant_id>/edit/')
+def editRestaurant(restaurant_id):
+    return 'edit specific restaurant'
+
+
+@app.route('/restaurant/<int:restaurant_id>/delete/')
+def deleteRestaurant(restaurant_id):
+    return 'delete specific restaurant'
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/')
+def menuItems(restaurant_id, menu_id):
+    return 'list all menu items for specific restaurant menu'
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return 'edit specific menu item'
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return 'delete specific menu item'
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
